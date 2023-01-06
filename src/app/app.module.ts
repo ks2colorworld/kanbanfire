4d172d2a8cd522b0ec622028c0a75515fbcc2dcd
mikey lee <ks@colorworld.kr>
2023-01-07 00:35:08 +0900

    modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts 	new file:   src/app/task-dialog/TaskDialogData.ts 	new file:   src/app/task-dialog/TaskDialogResult.ts 	new file:   src/app/task-dialog/task-dialog.component.css 	new file:   src/app/task-dialog/task-dialog.component.html 	new file:   src/app/task-dialog/task-dialog.component.spec.ts 	new file:   src/app/task-dialog/task-dialog.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,19 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from "@angular/material/card";
 import { DragDropModule } from "@angular/cdk/drag-drop";
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 import { TaskComponent } from './task/task.component';
+import { TaskDialogComponent } from './task-dialog/task-dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskComponent
+    TaskComponent,
+    TaskDialogComponent
   ],
   imports: [
     BrowserModule,
@@ -23,6 +29,10 @@ import { TaskComponent } from './task/task.component';
     MatIconModule,
     MatCardModule,
     DragDropModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatInputModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
